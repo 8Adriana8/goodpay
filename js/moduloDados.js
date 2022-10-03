4d121c212edd8442eccc6ab28d29bd91f0f44d88
8Adriana8 <acls.records@gmail.com>
2022-10-03 17:06:10 -0300

    03.10 - usuario e senha, array, funcao

---
+++ js/moduloDados.js
@@ -0,0 +1,37 @@
+
+/**Dentro deste array existem usuarios com essa estrutura:
+ * - id
+ *  - nome usuário
+ * -  senha
+ *  -  email
+ * 
+ */
+const usuarios = [
+    {
+        id: 1,
+        nomeUsuario: "adriana-banana",
+        senha: "12345",
+        email: "adriana.banana@soulcodeacademy.org"
+    },
+    {
+        id: 2,
+        nomeUsuario: "renato3x",
+        senha: "12345",
+        email: "renato.pereirar@gmail.com"
+    },
+    {
+        id: 3,
+        nomeUsuario: "bruno-dias",
+        senha: "12345",
+        email: "brunos.dias@gmail.com"
+    },
+    {
+        id: 4,
+        nomeUsuario: "Maria-Joana",
+        senha: "12345",
+        email: "majo@hotmail.com"
+    },
+]
+
+export default usuarios;
+/**Disponibiliza o array para ser utilizado fora do arquivo moduloDados.js */
